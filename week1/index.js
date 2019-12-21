1f7a47030ae113f81eb7648b8b79f808b4edcf9e
FernandoABarata <fernando.azevedobara@bryanuniversity.edu>
2019-12-21 15:23:32 -0800

    Bronze

---
+++ week1/index.js
@@ -0,0 +1,10 @@
+var shopper = {
+    firstName: "Fernando",
+    lastName: "Azevedo Barata",
+    age: 25,
+    isAlive: true,
+    fullName: function() {
+        return this.firstName + " " + this.lastName;
+    }
+    groceryCart: ["steak", "potatoes", "lettuce", "apples"];
+};
---
+++ week2/if-statement-olympics/index.js
@@ -0,0 +1,30 @@
+if(5 > 3) {
+    console.log ("Is greater then!");
+}
+
+if( "cat".length === 3) {
+    console.log ("Is the length!");
+}
+
+if("cat" === "dog") {
+    console.log ("They are the same!");
+} else {
+    console.log ("Not the same!");
+}
+
+var person = {
+    name: "Bobby",
+    age: 12
+}
+
+if("Bobby".age > 18) {
+    console.log ("Bobby is allowed to go to the movie!");
+} else {
+    console.log ("Bobby is not allowed to go to the movie!");
+}
+
+if(("Bobby".charAt(0) === "B") && ("Bobby" > 18)) {
+    console.log ("Bobby is allowed to go to the movie!");
+} else {
+    console.log ("Bobby is not allowed to go to the movie")
+}
